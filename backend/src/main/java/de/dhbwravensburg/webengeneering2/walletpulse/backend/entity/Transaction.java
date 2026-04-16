40733869767dd5f268790b5ecd2b37843604e3bb
WSukram <84n8qjp96j@privaterelay.appleid.com>
2026-04-16 14:33:43 +0200

    Complete domain model with Wallet, Asset and Transaction entities

---
+++ backend/src/main/java/de/dhbwravensburg/webengeneering2/walletpulse/backend/entity/Transaction.java
@@ -0,0 +1,36 @@
+package de.dhbwravensburg.webengeneering2.walletpulse.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Associated Assets
+    @ManyToOne
+    @JoinColumn(name = "asset_id", nullable = false)
+    private Asset asset;
+
+    // Amount (e.g. 0.5 BTC)
+    @Column(nullable = false)
+    private double amount;
+
+    // Purchase price per unit
+    @Column(nullable = false)
+    private double buyPrice;
+
+    // Purchase date
+    @Column(nullable = false)
+    private LocalDate date;
+}
