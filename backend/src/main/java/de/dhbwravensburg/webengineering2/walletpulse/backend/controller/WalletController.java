8ed20ed9b7345cc95fbe0de25b1f248ece158191
WSukram <84n8qjp96j@privaterelay.appleid.com>
2026-04-22 22:54:47 +0200

    Added WalletController with Get Request to all wallets and wallet id

---
+++ backend/src/main/java/de/dhbwravensburg/webengineering2/walletpulse/backend/controller/WalletController.java
@@ -0,0 +1,31 @@
+package de.dhbwravensburg.webengineering2.walletpulse.backend.controller;
+
+import de.dhbwravensburg.webengineering2.walletpulse.backend.entity.Wallet;
+import de.dhbwravensburg.webengineering2.walletpulse.backend.service.WalletService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/wallets")
+public class WalletController {
+
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @GetMapping
+    public List<Wallet> getAllWallets(){
+        return walletService.getAllWallets();
+    }
+
+    @GetMapping("/{id}")
+    public Wallet getWalletById(@PathVariable Long id) {
+        return walletService.getWalletById(id);
+    }
+}
