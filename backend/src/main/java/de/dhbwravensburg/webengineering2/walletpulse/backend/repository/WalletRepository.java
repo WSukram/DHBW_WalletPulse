048d7265fd1aa2f60cb646157738778336d480c3
WSukram <84n8qjp96j@privaterelay.appleid.com>
2026-04-22 22:38:41 +0200

    Implement CRUD operation in WalletService

--- backend/src/main/java/de/dhbwravensburg/webengineering2/walletpulse/backend/repository/WalletRepository.java
+++ backend/src/main/java/de/dhbwravensburg/webengineering2/walletpulse/backend/repository/WalletRepository.java
@@ -3,5 +3,8 @@ package de.dhbwravensburg.webengineering2.walletpulse.backend.repository;
 import de.dhbwravensburg.webengineering2.walletpulse.backend.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WalletRepository extends JpaRepository <Wallet, Long> {
+    List<Wallet> id(Long id);
 }
