1f9e6b41a50dd9ae7037e73ccfd67e8c274a5047
WSukram <84n8qjp96j@privaterelay.appleid.com>
2026-04-23 23:35:47 +0200

    feat(backend): add asset domain validation and service logic

--- backend/src/main/java/de/dhbwravensburg/webengineering2/walletpulse/backend/repository/AssetRepository.java
+++ backend/src/main/java/de/dhbwravensburg/webengineering2/walletpulse/backend/repository/AssetRepository.java
@@ -3,5 +3,8 @@ package de.dhbwravensburg.webengineering2.walletpulse.backend.repository;
 import de.dhbwravensburg.webengineering2.walletpulse.backend.entity.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AssetRepository extends JpaRepository <Asset, Long> {
+	List<Asset> findByWalletId(Long walletId);
 }
